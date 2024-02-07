c3a9af7f2144cdd97cfdf067c0e2f2da620c60a0
FalihaAbdulSalam <falihaabdulsalam@gmail.com>
2024-02-08 03:45:31 +0530

    change vue

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,12 @@
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/simple-vuejs-app/'
     : '/',
   configureWebpack: {
     optimization: {
-      minimizer: [
-        new TerserPlugin({
-          terserOptions: {
-            ecma: undefined,
-            warnings: false,
-            parse: {},
-            compress: {},
-            mangle: true, // Note `mangle.properties` is `false` by default.
-            module: false,
-            output: null,
-            toplevel: false,
-            nameCache: null,
-            ie8: false,
-            keep_fnames: false,
-          },
-        }),
-      ],
+      minimizer: [],
     },
   },
 };

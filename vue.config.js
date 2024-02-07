5ed783bfa673f187b776da655a6e0b7bfba87311
FalihaAbdulSalam <falihaabdulsalam@gmail.com>
2024-02-08 03:39:40 +0530

    changed

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,26 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/simple-vuejs-app/'
-    : '/'
-}
+    : '/',
+  configureWebpack: {
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            ecma: undefined,
+            warnings: false,
+            parse: {},
+            compress: {},
+            mangle: true, // Note `mangle.properties` is `false` by default.
+            module: false,
+            output: null,
+            toplevel: false,
+            nameCache: null,
+            ie8: false,
+            keep_fnames: false,
+          },
+        }),
+      ],
+    },
+  },
+};

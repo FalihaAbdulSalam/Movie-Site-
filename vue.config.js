b156dbc9118538b4fb089e8b553dca97a0b427ac
FalihaAbdulSalam <falihaabdulsalam@gmail.com>
2024-02-08 04:17:20 +0530

    change

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,5 @@
-const TerserPlugin = require('terser-webpack-plugin');
-
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/simple-vuejs-app/'
-    : '/',
-  configureWebpack: {
-    optimization: {
-      minimizer: [],
-    },
-  },
-};
+    : '/'
+}
